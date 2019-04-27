27b9790eacec6f7fd7e0e7844e1af3d47ca78cfd
dittops <dittops@accubits.com>
2019-04-27 21:56:09 +0530

    Spinner run from local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-// import {SpinnerModule} from '../../dist/spinner';
-import {SpinnerModule} from '@accubits/spinner';
+import {SpinnerModule} from '../../dist/spinner';
+// import {SpinnerModule} from '@accubits/spinner';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
